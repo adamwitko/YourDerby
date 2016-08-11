fa97f8a9f4f3a003fdfcdad0d6fb9bcaf37d45ee
Adam Witko <adam.witko@gmail.com>
2016-08-11 20:54:24 +0100

    Fix reference angular dependencies that were no longer needed

--- src/vendor.ts
+++ src/vendor.ts
@@ -5,6 +5,3 @@ import '@angular/common';
 import '@angular/http';
 import '@angular/router';
 import 'rxjs';
-import '@angular2-material/core';
-import '@angular2-material/card';
-import '@angular2-material/button';
